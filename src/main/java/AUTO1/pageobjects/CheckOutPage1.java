d64727b162796e8519ccb6651c7ce3f38c134d40
R-SProjects <gadgets785@gmail.com>
2026-06-15 14:42:18 +0530

    Removed testng import for cartPage and CheckOutPage

--- src/main/java/AUTO1/pageobjects/CheckOutPage1.java
+++ src/main/java/AUTO1/pageobjects/CheckOutPage1.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.testng.Assert;
-
 import AUTO1.Abstractcomponents.AbstractComponent1;
 
 public class CheckOutPage1 extends AbstractComponent1{
