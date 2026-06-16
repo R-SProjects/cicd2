7198431b42d3786a5fd86e34513c5aeaacbbce96
R-SProjects <gadgets785@gmail.com>
2026-06-16 18:05:59 +0530

    src/test/java/cucumber/TestNGTestRunner.java

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 //@CucumberOptions(features="\\src\\test\\java\\cucumber\\SubmitOrder.feature",glue="AUTO1\\StepDefinitions",monochrome=true,plugin={"html:\\LatestRep\\cucumber.html"})
-@CucumberOptions(features="src/test/java/cucumber",glue="AUTO1/StepDefinitions",monochrome=true,tags="@Regression",plugin= {"html:LatestRep/cucumberCC.html"})
+@CucumberOptions(features="src/test/java/cucumber",glue="AUTO1/StepDefinitions",monochrome=true,tags="@Regression",plugin= {"html:target/LatestRep/cucumberCC.html"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 
 }
